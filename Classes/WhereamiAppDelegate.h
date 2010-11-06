543708bc0c9c3e80479ac464844768fb4200eab4
beepscore <support@beepscore.com>
2010-11-06 00:45:05 -0700

    Initial commit.  Reference "iPhone Programming - The Big Nerd Ranch Guide" by Conway and Hillegass Ch 04-05.

---
+++ Classes/WhereamiAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  WhereamiAppDelegate.h
+//  Whereami
+//
+//  Created by Steve Baker on 11/6/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Whereami
+//
+//  Created by Steve Baker on 11/6/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
