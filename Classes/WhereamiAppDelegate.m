9ff7c1c9cb8781df757260c3b85f60b50d771c5b
beepscore <support@beepscore.com>
2010-11-13 23:08:36 -0800

    Log location to console.  Location does not run in simulator, must run on device.

--- Classes/WhereamiAppDelegate.m
+++ Classes/WhereamiAppDelegate.m
@@ -16,17 +16,32 @@
 #pragma mark -
 #pragma mark Application lifecycle
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{    
+    // Create location manager object
+    locationManager = [[CLLocationManager alloc] init];
     
-    // Override point for customization after application launch.
+    // Make this instance of WhereamiAppDelegate the delegate
+    // it will send its messages to our WhereamiAppDelegate
+    [locationManager setDelegate:self];
+    
+    // We wan all results from the location manager
+    [locationManager setDistanceFilter:kCLDistanceFilterNone];
+    
+    // And we want it to be as accurate as possible
+    // regardless of how much time/power it takes
+    [locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
+    
+    // Tell our manager to start looking for its location immediately
+    [locationManager startUpdatingLocation];
     
     [window makeKeyAndVisible];
-    
     return YES;
 }
 
 
-- (void)applicationWillResignActive:(UIApplication *)application {
+- (void)applicationWillResignActive:(UIApplication *)application
+{
     /*
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -34,7 +49,8 @@
 }
 
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
     /*
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
@@ -42,21 +58,24 @@
 }
 
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
     /*
      Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
      */
 }
 
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
 }
 
 
-- (void)applicationWillTerminate:(UIApplication *)application {
+- (void)applicationWillTerminate:(UIApplication *)application 
+{
     /*
      Called when the application is about to terminate.
      See also applicationDidEnterBackground:.
@@ -67,17 +86,35 @@
 #pragma mark -
 #pragma mark Memory management
 
-- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application
+{
     /*
      Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
      */
 }
 
 
-- (void)dealloc {
+- (void)dealloc
+{
+    [locationManager setDelegate:nil];
     [window release];
     [super dealloc];
 }
 
+#pragma mark -
+#pragma mark Core Location delegate methods
+
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation
+{
+    NSLog(@"%@", newLocation);
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+didFailWithError:(NSError *)error
+{
+    NSLog(@"Could not find location: %@", error);
+}
 
 @end
